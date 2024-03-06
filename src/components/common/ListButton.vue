a160af3ebb571cf8ff587b60a08fb3bb74c00002
Artem Honcharenko <artemgonchar7@gmail.com>
2024-03-06 23:29:07 +0100

    feat: added ListButton & PriorityMark components

---
+++ src/components/common/ListButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :class="`w-10 h-10 rounded-md leading-5 ${bgColor} active:opacity-40`">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ListButton',
+  props: {
+    bgColor: String
+  }
+})
+</script>
