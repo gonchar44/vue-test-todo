d9b664a26497514ca939d35e77e7d2d69ba9c801
Artem Honcharenko <artemgonchar7@gmail.com>
2024-03-08 10:24:24 +0100

    feat: added Not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,23 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import MainView from '../views/MainView.vue'
+import WelcomeView from '@/views/WelcomeView.vue'
+import MainView from '@/views/MainView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'welcome',
-    component: () => import('@/views/WelcomeView.vue')
+    component: WelcomeView
   },
   {
     path: '/main',
     name: 'main',
     component: MainView
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFoundView
   }
 ]
 
